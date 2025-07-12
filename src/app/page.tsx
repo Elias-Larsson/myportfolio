2c4bbbe0af61ba1962b521a1ad3ae3e680efa10e
Elias-Larsson <aroezi05@gmail.com>
2025-07-12 20:14:03 +0200

    feat: add background for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import Image from 'next/image'
-export default function Home() {
+import { Navbar } from "./components/navbar";
 
+export default function Home() {
   return (
-  <>
-    <h1>Elias Larsson</h1>
-    <Image alt="idc" width="64" height="64" src="lionicon.svg" />
-   <p>coming soon...</p>
-   <a href="https://github.com/Elias-Larsson">github</a>
-   </>
-  )
+    <>
+      <div className="flex flex-col items-center bg-[url('/wavebackground.svg')] bg-cover bg-center bg-no-repeat w-screen h-screen">
+        <Navbar></Navbar>
+        <div className="flex flex-col items-center justify-center flex-1">
+          <h1>Elias Larsson</h1>
+          <p>coming soon...</p>
+          <a href="https://github.com/Elias-Larsson">github</a>
+        </div>
+      </div>
+    </>
+  );
 }
