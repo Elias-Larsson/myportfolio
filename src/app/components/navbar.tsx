f874d91cc23cdfbe3f96f88eb38e540988d88331
Elias-Larsson <aroezi05@gmail.com>
2025-07-13 16:45:17 +0200

    fix: fix navbar so it's clickable

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -12,23 +12,31 @@ export const Navbar = () => {
     { name: "Projects", href: "/projects" },
     { name: "Contact", href: "/contact" },
   ];
-  const current = menuitems.find((item) => item.href === pathname)?.name || null;
+  const current =
+    menuitems.find((item) => item.href === pathname)?.name || null;
   const [hovered, setHovered] = useState<string | null>(current);
 
   return (
-    <nav onMouseLeave={() => setHovered(current)} className="pt-4 sticky top-0 z-50">
+    <nav
+      onMouseLeave={() => setHovered(current)}
+      className="pt-4 sticky top-0 z-50"
+    >
       <ul className="bg-neutral-300/30 bg-blur p-2 flex rounded-4xl gap-2 backdrop-blur-[4px] shadow-lg">
         {menuitems.map((item) => (
-          <li
-            onMouseEnter={() => setHovered(item.name)}
-            key={item.name}
-            className="navbarList relative cursor-pointer"
-          >
-            <Link href={item.href}>{item.name}</Link>
-            {hovered === item.name && (
-              <motion.div className="bg-neutral-300/30 absolute inset-0 rounded-4xl" layoutId="hover"></motion.div>
-            )}
-          </li>
+          <Link key={item.name} href={item.href}>
+            <li
+              onMouseEnter={() => setHovered(item.name)}
+              className="navbarList relative cursor-pointer"
+            >
+              {item.name}
+              {hovered === item.name && (
+                <motion.div
+                  className="bg-neutral-300/30 absolute inset-0 rounded-4xl"
+                  layoutId="hover"
+                ></motion.div>
+              )}
+            </li>
+          </Link>
         ))}
       </ul>
     </nav>
