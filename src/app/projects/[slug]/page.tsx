6f85ffed8122eb419b8cce2b57f40701bb43376f
Elias-Larsson <aroezi05@gmail.com>
2025-07-15 01:57:38 +0200

    refactor: add render checks for slug pages

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -7,8 +7,9 @@ import { groq, SanityDocument } from "next-sanity";
 import { client } from "@/sanity/lib/client";
 import { Banner } from "@/app/components/backgroundbanner";
 import { Techstacks } from "@/app/components/techstacks";
-import Image from "next/image";
 import { Button } from "@/app/components/button";
+import { urlFor } from "@/sanity/lib/image";
+import Image from "next/image";
 
 export default function SlugProjectPage() {
   const { slug } = useParams();
@@ -25,6 +26,7 @@ export default function SlugProjectPage() {
         longDescription,
         projectImage,
         backgroundImage,
+        previewImage,
         repoLink,
         liveDemoLink,
         video {
@@ -59,36 +61,41 @@ export default function SlugProjectPage() {
         <div className="flex flex-col  gap-4 px-4">
           <h1 className="text-4xl">{project.title}</h1>
           <div className="flex flex-row gap-2">
-            <Button
-              name="Live Demo"
-              route={project.liveDemoLink}
-              decoration="/maki_arrow.svg"
-            />
-            <Button decoration="/mdi_github.svg" route={project.repoLink}/>
+            {project.liveDemoLink && (
+              <Button
+                name="Live Demo"
+                route={project.liveDemoLink}
+                decoration="/maki_arrow.svg"
+              />
+
+            )}
+            <Button decoration="/mdi_github.svg" route={project.repoLink} />
           </div>
           <p className="max-w-128 text-neutral-400">{project.description}</p>
           <p className="text-base/7 max-w-152">{project.longDescription}</p>
         </div>
         <div className="flex flex-col items-center gap-4 max-w-128">
-          {project.video && (
+          {project.video ? (
             <video
-              src={project.video.asset.url || project.previewImage}
+              src={project.video.asset.url}
               className="object-cover object-top w-96"
               autoPlay
               muted
               loop
             />
+          ) : (
+            project.previewImage && (
+              <Image
+                src={urlFor(project.previewImage).url()}
+                alt="Project preview"
+                width={384}
+                height={384}
+                className="object-cover object-top w-96 max-h-128"
+              />
+            )
           )}
           <Techstacks techstacks={project.techstack} />
         </div>
-        {project.previewImage && (
-          <Image
-            src={project.previewImage}
-            alt="preview image"
-            width={120}
-            height={120}
-          />
-        )}
       </main>
     </>
   );
