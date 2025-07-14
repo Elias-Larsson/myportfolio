23007755c7bbcb05b75aa1da3255aa85800243d8
Elias-Larsson <aroezi05@gmail.com>
2025-07-14 22:21:18 +0200

    feat: add techstacks component

---
+++ src/app/components/techstacks.tsx
@@ -0,0 +1,18 @@
+
+type Props = {
+    techstacks: string[]
+}
+export const Techstacks = ({techstacks}: Props) => {
+    return (
+          <div className="flex flex-wrap justify-center gap-3 p-4">
+            {techstacks?.map((techstack: string) => (
+              <span
+                key={techstack}
+                className="bg-red-900 px-4 py-2 rounded-xl hover:bg-red-950 transition duration-200 ease-in-out cursor-default"
+              >
+                {techstack}
+              </span>
+            ))}
+          </div>
+    )
+} 
