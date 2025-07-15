01980a9ff7e0fbdd733d6b914078615ee25226c3
Elias-Larsson <aroezi05@gmail.com>
2025-07-15 18:12:05 +0200

    feat: add queries

---
+++ src/sanity/lib/queries.tsx
@@ -0,0 +1,38 @@
+import { groq } from "next-sanity";
+export const homepageQuery = groq`*[_type == "homepage"]{
+  title, 
+  subtitle, 
+  description, 
+  skills,
+  }[0]`;
+export const aboutQuery = groq`*[_type=="about"]{
+  description, 
+  "profile": profileImage.asset->url,
+}[0]`;
+
+export const footerQuery = groq`*[_type == "homepage"]{
+    contactDescription
+}`;
+
+export const projectsQuery = groq`*[_type == "project"]{
+  _id, 
+  title, 
+  definition, 
+  slug, 
+  description,
+  "projectImage": projectImage.asset->url
+ }`;
+
+export const projectQuery = groq`*[_type == "project" && slug.current == $slug][0]{
+  _id,
+  title,
+  description,
+  longDescription,
+  slug,
+  techstack,
+  liveDemoLink,
+  repoLink,
+  "backgroundImage": backgroundImage.asset->url,
+  "previewImage": previewImage.asset->url,
+  "video": video.asset->url
+}`;
