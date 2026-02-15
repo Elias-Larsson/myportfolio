b98e20cc5cb0d3e567e40766ca2a843157352dbf
Elias Larsson <aroezi05@gmail.com>
2026-02-16 00:03:09 +0100

    fix navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -5,9 +5,9 @@ export const Navbar = () => {
 
   return (
     <nav className="flex flex-col sm:flex-row gap-4 items-center justify-center">
-      <Button route="about">about me</Button>
+      <Button route="/about">about me</Button>
       <Button route="/">home</Button>
-      <Button route="projects">projects</Button>
+      <Button route="/projects">projects</Button>
     </nav>
   );
 };
